a5a4aa523531f1dd7d35dc23041f6a0e35417420
huisunan <571101320@qq.com>
2020-04-23 17:20:57 +0800

    Initial commit

---
+++ src/main/java/com/hsn/Start.java
@@ -0,0 +1,23 @@
+package com.hsn;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Scanner;
+
+public class Start {
+    public static void main(String[] args) throws InterruptedException {
+        System.getProperties().setProperty("webdriver.chrome.driver","F:\\workspace\\idea\\selenuim\\src\\main\\resources\\chromedriver.exe");
+        Work work = new Work();
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            String next = scanner.next();
+            if ("q".equals(next)){
+                work.quit();
+                break;
+            }else if ("s".equals(next)){
+                work.start();
+            }
+        }
+    }
+}
