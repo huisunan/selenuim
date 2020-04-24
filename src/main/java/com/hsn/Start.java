8798f834019b792f61c6bf5ba3a1e87f57b310f5
huisunan <571101320@qq.com>
2020-04-24 15:27:20 +0800

    12312

--- src/main/java/com/hsn/Start.java
+++ src/main/java/com/hsn/Start.java
@@ -7,19 +7,20 @@ import java.net.URL;
 import java.util.Scanner;
 
 public class Start {
-    public static void main(String[] args) throws InterruptedException {
-        String s = Start.class.getResource("chromedriver.exe").toString();
-        System.getProperties().setProperty("webdriver.chrome.driver","F:\\workspace\\idea\\selenuim\\src\\main\\resources\\chromedriver.exe");
+    public static void main(String[] args){
+        System.getProperties().setProperty("webdriver.chrome.driver", "chromedriver.exe");
         Work work = new Work();
         Scanner scanner = new Scanner(System.in);
-        while (true){
+        System.out.println("进入网页后，输入's'开始：");
+        while (true) {
             String next = scanner.next();
-            if ("q".equals(next)){
+            if ("q".equals(next)) {
                 work.quit();
                 break;
-            }else if ("s".equals(next)){
+            } else if ("s".equals(next)) {
                 work.start();
             }
         }
+
     }
 }
--- src/main/java/com/hsn/Work.java
+++ src/main/java/com/hsn/Work.java
@@ -1,22 +1,21 @@
 package com.hsn;
 
-import org.omg.PortableServer.THREAD_POLICY_ID;
 import org.openqa.selenium.*;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.Select;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Work {
     private WebDriver webDriver;
     private static final String url = "http://i.mooc.chaoxing.com/";
+    private String title;
+    private String value;
 
-    public Work(){
+    public Work() {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("user-data-dir=C:\\User Data");
         this.webDriver = new ChromeDriver(chromeOptions);
@@ -24,175 +23,204 @@ public class Work {
         this.webDriver.get(url);
     }
 
-    public void quit(){
+    public void quit() {
         this.webDriver.quit();
     }
 
-    public void start() throws InterruptedException {
-        Set<String> handles = webDriver.getWindowHandles();
-        String[] strings = handles.toArray(new String[0]);
-        this.webDriver.switchTo().window(strings[0]);
-        Thread.sleep(3000);
-        try {
-            //获取课程列表
-            List<WebElement> ncells = webDriver.findElements(By.cssSelector(".ncells"));
-            for (WebElement cell : ncells) {
-                WebElement student = cell.findElement(By.cssSelector(".roundpointStudent"));
-                //课程已经完成
-                if (!"2".equals(student.getText())){
-                    continue;
-                }
-                Actions actions = new Actions(webDriver);
+    private void moveAndClick(WebElement element) {
+        Dimension size = element.getSize();
+        int xOffset = (int) (size.getWidth() * (1 - Math.random() * 2) / 2);
+        int yOffset = (int) (size.getHeight() * (1 - Math.random() * 2) / 2);
+        new Actions(webDriver).moveToElement(element, xOffset, yOffset).click().perform();
+    }
 
-                actions.moveToElement(cell).click(cell).perform();
+    private void moveAndClickVideo(WebElement video) {
+        Dimension size = video.getSize();
+        int xOffset = (int) (size.getWidth() * (1 - Math.random() * 2) / 2);
+        int yOffset = (int) ((size.getHeight() - 100) * (1 - Math.random() * 2) / 2);
+        new Actions(webDriver).moveToElement(video, xOffset, yOffset).click().perform();
+    }
+
+    /**
+     * 切换到视频tab页
+     */
+    private void clickTab() throws InterruptedException {
+        //获取tab页
+        WebElement tab = webDriver.findElement(By.cssSelector(".tabtags"));
+        List<WebElement> spans = tab.findElements(By.cssSelector("span"));
+        for (WebElement span : spans) {
+            String title = span.getAttribute("title");
+            if ("视频".equals(title)) {
+                //点击tab页
+                moveAndClick(span);
                 Thread.sleep(1000);
-
-                //获取tab页
-                WebElement tab = webDriver.findElement(By.cssSelector(".tabtags"));
-                List<WebElement> spans = tab.findElements(By.cssSelector("span"));
-                for (WebElement span : spans) {
-                    String title = span.getAttribute("title");
-                    if ("视频".equals(title)){
-                        //点击tab页
-                        Actions temp = new Actions(webDriver);
-                        temp.moveToElement(span).perform();
-                        Thread.sleep(500);
-                        temp.click(span).perform();
-                        Thread.sleep(500);
-                        break;
-                    }
-                }
-
-                /**
-                 * 滚动进度条
-                 */
-                ((JavascriptExecutor)webDriver).executeScript("window.scrollTo(0,250);");
-
-
-                //进入 iframe
-                WebElement iframe = webDriver.findElement(By.tagName("iframe"));
-                webDriver.switchTo().frame(iframe);
-                Thread.sleep(500);
-                WebElement iframe2 = webDriver.findElement(By.tagName("iframe"));
-                webDriver.switchTo().frame(iframe2);
-                //点击播放按钮
-                WebElement videoButton = webDriver.findElement(By.cssSelector(".vjs-big-play-button"));
-                Actions videoAction = new Actions(webDriver);
-                videoAction.moveToElement(videoButton).perform();
-                Thread.sleep(500);
-                videoAction.click(videoButton).perform();
-                Thread.sleep(500);
-                //获取视频播放器
-                WebElement video = webDriver.findElement(By.cssSelector("#video_html5_api"));
-
-                Thread.sleep(300);
-                Dimension videoSize = video.getSize();
-                //移动到播放器中间
-                videoAction.moveToElement(video,videoSize.getWidth()/2,videoSize.getHeight()/2).perform();
-                Thread.sleep(500);
-                //暂停
-                /*videoAction.click(video).perform();
-                Thread.sleep(500);
-                //切换二倍速
-                List<WebElement> elements = webDriver.findElements(By.cssSelector(".vjs-playback-rate"));
-                for (WebElement element : elements) {
-                    if ("播放速度".equals(element.getAttribute("title"))){
-                        videoAction.moveToElement(element).perform();
-                        Thread.sleep(500);
-                        List<WebElement> items = webDriver.findElements(By.cssSelector(".vjs-menu-item-text"));
-                        for (WebElement item : items) {
-                            if ("2x".equals(item.getText())){
-                                videoAction.click(item);
-                                break;
-                            }
-                        }
-                        break;
-                    }
-                }
-                //移动到播放器中间
-                videoAction.moveToElement(video,videoSize.getWidth()/2,videoSize.getHeight()/2).perform();
-                Thread.sleep(500);*/
-                //播放
-                videoAction.click(video).perform();
-                Thread.sleep(500);
-                //监听滚动跳
-                WebElement progress = webDriver.findElement(By.cssSelector(".vjs-progress-holder"));
-                String value = progress.getAttribute("aria-valuenow");
-
-                while (!value.startsWith("100")){
-                    boolean haveAns = false;
-                    //出现答题卡
-                    try{
-                        WebElement submit = webDriver.findElement(By.cssSelector(".ans-videoquiz-submit"));
-                        haveAns = true;
-                        WebElement opts = webDriver.findElement(By.cssSelector(".ans-videoquiz-opts"));
-                        List<WebElement> labels = opts.findElements(By.tagName("label"));
-                        int count = 0;
-                        for (WebElement label : labels) {
-
-                            WebElement input = label.findElement(By.tagName("input"));
-                            JavascriptExecutor javascriptExecutor = (JavascriptExecutor) webDriver;
-                            if (input.getAttribute("value").equals("true")){
-                                Actions temp = new Actions(webDriver);
-                                temp.moveToElement(input).perform();
-                                Thread.sleep(500);
-                                temp = new Actions(webDriver);
-                                temp.click(input).perform();
-                                Thread.sleep(500);
-                                temp = new Actions(webDriver);
-                                temp.moveToElement(submit).perform();
-                                Thread.sleep(500);
-                                temp.click(submit).perform();
-                               /* javascriptExecutor.executeAsyncScript("document.querySelectorAll(\"input[name='ans-videoquiz-opt']\")["+count+"].click()");
-                                Thread.sleep(500);
-                                javascriptExecutor.executeAsyncScript("document.querySelector(\".ans-videoquiz-submit\").click()");
-                                Thread.sleep(500);*/
-                            }else {
-                                count++;
-                                continue;
-                            }
-
-                            boolean flag =true;
-                            while (true){
-                                try{
-                                    //警告框接受
-                                    Alert alert = webDriver.switchTo().alert();
-                                    alert.accept();
-                                    Thread.sleep(500);
-                                    //回答错误
-                                    flag = false;
-                                }catch (NoAlertPresentException e){
-                                    break;
-                                }
-                            }
-                            //回答正确
-                            if (flag){
-                                break;
-                            }
-                        }
-                    }catch (NoSuchElementException e){
-                    }
-                    Thread.sleep(500);
-                    if (!haveAns){
-                        try{
-                            //视频被暂停
-                            webDriver.findElement(By.cssSelector(".vjs-paused"));
-                            videoAction.click(video).perform();
-                        }catch (NoSuchElementException e){
-
-                        }
-                    }
-
-                    Thread.sleep(1000);
-                    value = progress.getAttribute("aria-valuenow");
-                }
-
-                //推出iframe
-                webDriver.switchTo().defaultContent();
+                break;
             }
-            System.out.println(ncells);
-        }catch (Exception e){
-            System.out.println(e.getMessage());
         }
     }
+
+    private void playVideo() throws InterruptedException {
+        /**
+         * 滚动进度条
+         */
+        ((JavascriptExecutor) webDriver).executeScript("window.scrollTo(0,250);");
+        Thread.sleep(300);
+        //进入 iframe
+        WebElement iframe = webDriver.findElement(By.tagName("iframe"));
+        webDriver.switchTo().frame(iframe);
+        Thread.sleep(500);
+        WebElement iframe2 = webDriver.findElement(By.tagName("iframe"));
+        webDriver.switchTo().frame(iframe2);
+        Thread.sleep(500);
+        //点击播放按钮
+        WebElement videoButton = webDriver.findElement(By.cssSelector(".vjs-big-play-button"));
+        moveAndClickVideo(videoButton);
+        Thread.sleep(500);
+    }
+
+    /**
+     * 自动答题
+     */
+    private boolean fillAnswer() {
+        boolean result = false;
+        try {
+
+            WebElement opts = webDriver.findElement(By.cssSelector(".ans-videoquiz-opts"));
+            result = true;
+            List<WebElement> labels = opts.findElements(By.tagName("label"));
+            for (WebElement label : labels) {
+                WebElement input = label.findElement(By.tagName("input"));
+                if (input.getAttribute("value").equals("true")) {
+                    moveAndClick(input);
+                    Thread.sleep(1000);
+                    WebElement submit = webDriver.findElement(By.cssSelector(".ans-videoquiz-submit"));
+                    moveAndClick(submit);
+                    Thread.sleep(500);
+                    break;
+                }
+            }
+        } catch (NoSuchElementException | InterruptedException ignored) {
+        }
+        return result;
+    }
+
+    /**
+     * 视频被暂停  恢复
+     */
+    private void recoveryVideo(boolean haveAns) {
+        if (!haveAns) {
+            try {
+                //视频被暂停
+                WebElement video = webDriver.findElement(By.cssSelector("#video_html5_api"));
+
+                webDriver.findElement(By.cssSelector(".vjs-paused"));
+                moveAndClickVideo(video);
+            } catch (NoSuchElementException ignored) {
+
+            }
+        }
+    }
+
+    /**
+     * 检测视频是否结束
+     */
+    private void listener() throws InterruptedException {
+        //获取进度条
+        WebElement progress = webDriver.findElement(By.cssSelector(".vjs-progress-holder"));
+        String value = progress.getAttribute("aria-valuenow");
+        //获取视频播放器
+        while (!value.startsWith("100")) {
+            //出现答题卡
+            boolean haveAns = fillAnswer();
+            recoveryVideo(haveAns);
+            Thread.sleep(1000);
+            //更新进度条的值
+            value = progress.getAttribute("aria-valuenow");
+            System.out.print("\r" + title + "     " + value + "%");
+        }
+        Thread.sleep(3000);
+
+    }
+
+    private void switchWindow() throws InterruptedException {
+        Set<String> handles = webDriver.getWindowHandles();
+        //窗口切换
+        boolean find = false;
+        for (String handle : handles) {
+            webDriver.switchTo().window(handle);
+            if (webDriver.getCurrentUrl().contains("/mycourse/studentstudy")) {
+                find = true;
+                break;
+            } else {
+                webDriver.close();
+            }
+        }
+        if (!find) {
+            throw new RuntimeException("没有找到学习窗口");
+        }
+        Thread.sleep(3000);
+    }
+
+    /**
+     * 开始学习模式
+     */
+    private void startStudy() throws InterruptedException {
+        //获取未完成的列表
+        while (true) {
+            webDriver.switchTo().defaultContent();
+            int offset = 0;
+            //content 33px
+            //ncells 27px
+            //document.querySelector("#content1").scrollTo(0,100)
+            List<WebElement> cells = webDriver.findElements(By.cssSelector(".cells"));
+            WebElement curCell = null;
+            boolean find = false;
+            for (WebElement cell : cells) {
+                if (find) {
+                    break;
+                }
+                offset += 33;
+                List<WebElement> elements = cell.findElements(By.cssSelector(".ncells"));
+                for (WebElement element : elements) {
+                    if ("2".equals(element.findElement(By.cssSelector(".roundpointStudent")).getText())) {
+                        curCell = element;
+                        find = true;
+                        break;
+                    }
+                    offset += 27;
+                }
+            }
+            if (curCell != null) {
+                ((JavascriptExecutor) webDriver).executeScript("document.querySelector(\"#content1\").scrollTo(0," + offset + ")");
+                Thread.sleep(800);
+                moveAndClick(curCell);
+                title = curCell.findElement(By.tagName("a")).getAttribute("title");
+                Thread.sleep(1000);
+                //切换选项卡
+                this.clickTab();
+                //播放视频
+                this.playVideo();
+                //开启监听
+                this.listener();
+            } else {
+                break;
+            }
+        }
+    }
+
+    public void start() {
+        try {
+            this.switchWindow();
+            this.startStudy();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("系统出错是否退出?\ny:退出");
+            String next = scanner.next();
+            if (next.equals("y")) {
+                webDriver.quit();
+            }
+        }
+    }
+
 }
